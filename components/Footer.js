63a8818beeb6e4f00444891a3ec448d3152b0bb0
amanantino <aman.singh@antino.com>
2025-02-19 12:51:39 +0530

    Navbar and Footer Ui Complete

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-900 text-white p-5 text-center'>
+      <p>Copyright &copy; Get Me a Chai! - All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <nav>
-      
-    </nav>
-  )
+    return (
+        <nav className='bg-gray-900 text-white flex justify-between p-5 px-32'>
+            <div className="logo text-2xl">GetMeAChai!!</div>
+            <ul className='flex justify-between gap-10'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Project</li>
+                <li>Sign Up</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
